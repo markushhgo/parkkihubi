712d57d31754d71e473275d579dee6730b8204aa
juuso-j <juuso.jokiniemi@turku.fi>
2023-10-30 15:36:13 +0200

    Add view and serializer for EventArea model

---
+++ parkings/api/public/event_area.py
@@ -0,0 +1,40 @@
+from django.utils import timezone
+from rest_framework import permissions, viewsets
+from rest_framework_gis.pagination import GeoJsonPagination
+from rest_framework_gis.serializers import (
+    GeoFeatureModelSerializer, GeometrySerializerMethodField)
+
+from parkings.models import EventArea
+
+from ..common import WGS84InBBoxFilter
+
+
+class AreaSerializer(GeoFeatureModelSerializer):
+    wgs84_areas = GeometrySerializerMethodField()
+
+    def get_wgs84_areas(self, area):
+        return area.geom.transform(4326, clone=True)
+
+    class Meta:
+        abstact = True
+        geo_field = 'wgs84_areas'
+        fields = (
+            'id',
+            'capacity_estimate',
+        )
+
+
+class EventAreaSerializer(AreaSerializer):
+
+    class Meta(AreaSerializer.Meta):
+        model = EventArea
+
+
+class PublicAPIEventAreaViewSet(viewsets.ReadOnlyModelViewSet):
+    permission_classes = [permissions.AllowAny]
+    queryset = EventArea.objects.filter(event_end__gte=timezone.now()).order_by('origin_id')
+    serializer_class = EventAreaSerializer
+    pagination_class = GeoJsonPagination
+    bbox_filter_field = 'geom'
+    filter_backends = (WGS84InBBoxFilter,)
+    bbox_filter_include_overlapping = True
