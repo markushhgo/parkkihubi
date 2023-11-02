8ba012f1fca73387f70adfe6764fc88c9d764a88
juuso-j <juuso.jokiniemi@turku.fi>
2023-11-02 08:29:40 +0200

    Add BaseParkingSerializer and EventParkingSerializer

--- parkings/api/monitoring/serializers.py
+++ parkings/api/monitoring/serializers.py
@@ -1,24 +1,19 @@
 import rest_framework_gis.serializers as gis_serializers
 from rest_framework import serializers
 
-from ...models import Parking, PaymentZone
+from ...models import EventParking, Parking, PaymentZone
 
 
-class ParkingSerializer(gis_serializers.GeoFeatureModelSerializer):
+class BaseParkingSerializer(gis_serializers.GeoFeatureModelSerializer):
     operator_name = serializers.CharField(
         source='operator.name', read_only=True)
-    zone = serializers.SlugRelatedField(
-        slug_field='code', queryset=PaymentZone.objects.all())
 
     class Meta:
-        model = Parking
+        abstract = True
         fields = [
             'id',
             'registration_number',
             'location',
-            'region',
-            'zone',
-            'terminal_number',
             'operator_name',
             'time_start',
             'time_end',
@@ -27,8 +22,28 @@ class ParkingSerializer(gis_serializers.GeoFeatureModelSerializer):
         ]
         geo_field = 'location'  # Stored already in SRID 4326 / WGS84
 
+
+class ParkingSerializer(BaseParkingSerializer):
+
+    zone = serializers.SlugRelatedField(
+        slug_field='code', queryset=PaymentZone.objects.all())
+
+    class Meta(BaseParkingSerializer.Meta):
+        model = Parking
+        fields = BaseParkingSerializer.Meta.fields + [
+            'region',
+            'zone',
+            'terminal_number',
+        ]
+
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         if instance.zone:
             representation['properties']['zone'] = instance.zone.casted_code
         return representation
+
+
+class EventParkingSerializer(BaseParkingSerializer):
+
+    class Meta(BaseParkingSerializer.Meta):
+        model = EventParking
