0e8464da92831b1169cb6784ad83f3af0414c5cb
juuso-j <juuso.jokiniemi@turku.fi>
2023-11-02 08:33:21 +0200

    Add ValidEventParkingViewSet

--- parkings/api/monitoring/urls.py
+++ parkings/api/monitoring/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 from ..url_utils import versioned_url
 from .region import RegionViewSet
 from .region_statistics import RegionStatisticsViewSet
+from .valid_event_parking import ValidEventParkingViewSet
 from .valid_parking import ValidParkingViewSet
 
 router = DefaultRouter()
@@ -11,6 +12,8 @@ router.register(r'region_statistics', RegionStatisticsViewSet,
                 basename='regionstatistics')
 router.register(r'valid_parking', ValidParkingViewSet,
                 basename='valid_parking')
+router.register(r'valid_event_parking', ValidEventParkingViewSet,
+                basename='valid_event_parking')
 
 app_name = 'monitoring'
 urlpatterns = [
