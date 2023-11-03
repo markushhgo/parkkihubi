d82aca165da0ec6d7150f35c038bac89f5e8ba25
juuso-j <juuso.jokiniemi@turku.fi>
2023-11-03 15:19:12 +0200

    Add OperatorAPIEventParkingViewSet

--- parkings/api/operator/urls.py
+++ parkings/api/operator/urls.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
 
 from ..url_utils import versioned_url
+from .event_parking import OperatorAPIEventParkingViewSet
 from .parking import OperatorAPIParkingViewSet
 from .permit import (
     OperatorActivePermitByExternalIdViewSet, OperatorPermitSeriesViewSet,
@@ -9,6 +10,7 @@ from .zone import PaymentZoneViewSet
 
 router = DefaultRouter()
 router.register(r'parking', OperatorAPIParkingViewSet, basename='parking')
+router.register(r'event_parking', OperatorAPIEventParkingViewSet, basename='eventparking')
 router.register(r'permitseries', OperatorPermitSeriesViewSet, basename='permitseries')
 router.register(r'permit', OperatorPermitViewSet, basename='permit')
 router.register(r'activepermit', OperatorActivePermitByExternalIdViewSet, basename='activepermit')
