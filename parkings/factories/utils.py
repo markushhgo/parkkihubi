1f4cf9de6d191279345c08d381c8d396e6bafde2
juuso-j <juuso.jokiniemi@turku.fi>
2023-11-13 13:17:17 +0200

    Add get_time_far_enough_in_past function

--- parkings/factories/utils.py
+++ parkings/factories/utils.py
@@ -1,3 +1,7 @@
+from datetime import timedelta
+
+import pytz
+
 from .faker import fake
 
 CAPITAL_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ'
@@ -7,3 +11,7 @@ def generate_registration_number():
     letters = ''.join(fake.random.choice(CAPITAL_LETTERS) for _ in range(3))
     numbers = ''.join(fake.random.choice('0123456789') for _ in range(3))
     return '%s-%s' % (letters, numbers)
+
+
+def get_time_far_enough_in_past():
+    return fake.date_time_this_decade(before_now=True, tzinfo=pytz.utc) - timedelta(days=7, seconds=1)
