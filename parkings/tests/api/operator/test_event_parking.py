b263edba6c097a5e38d4758a253bcdd76d6fa426
juuso-j <juuso.jokiniemi@turku.fi>
2023-11-21 10:59:18 +0200

    Test that event area is assigned by location.

--- parkings/tests/api/operator/test_event_parking.py
+++ parkings/tests/api/operator/test_event_parking.py
@@ -25,12 +25,12 @@ def new_event_parking_data():
         'registration_number': 'JLH-247',
         'time_start': '2016-12-10T20:34:38Z',
         'time_end': '2016-12-10T23:33:29Z',
-        'location': {'coordinates': [60.444052373652376, 22.29251289354579], 'type': 'Point'},
+        'location': {'coordinates': [24.90, 60.16], 'type': 'Point'},
     }
 
 
 @pytest.fixture
-def updated_event_parking_data():
+def event_parking_data_outside_event_areas():
     return {
         'registration_number': 'VSM-162',
         'time_start': '2016-12-12T20:34:38Z',
@@ -128,23 +128,20 @@ def test_patch_event_parking_event_area_id(operator_api_client, complete_event_p
     assert str(complete_event_parking.event_area.id) == new_event_area_id
 
 
-def test_post_event_parking(operator_api_client, new_event_parking_data):
+def test_post_event_parking(operator_api_client, new_event_parking_data, event_area):
     response_data = post(operator_api_client, list_url, new_event_parking_data)
     check_response_data(new_event_parking_data, response_data)
     new_event_parking = EventParking.objects.get(id=response_data['id'])
     check_data_matches_object(new_event_parking_data, new_event_parking)
+    # Test that event area assigned by location
+    assert new_event_parking.event_area.id == event_area.id
     # test location_gk25fin
     location_gk25fin = new_event_parking.location.transform(GK25FIN_SRID, clone=True)
     assert location_gk25fin.wkt == new_event_parking.location_gk25fin.wkt
 
 
-def test_put_event_parking_time_end_null(operator_api_client, event_parking, updated_event_parking_data):
-    detail_url = get_detail_url(event_parking)
-    updated_event_parking_data['time_end'] = None
-    response_data = put(operator_api_client, detail_url, updated_event_parking_data)
-    check_response_data(updated_event_parking_data, response_data)
-    event_parking.refresh_from_db()
-    check_data_matches_object(updated_event_parking_data, event_parking)
+def test_post_event_parking_without_event_area(operator_api_client, event_parking_data_outside_event_areas, event_area):
+    post(operator_api_client, list_url, event_parking_data_outside_event_areas, 400)
 
 
 def test_patch_event_parking(operator_api_client, event_parking):
