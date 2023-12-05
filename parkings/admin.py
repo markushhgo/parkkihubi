f68882ec3ea52b8b8846607bf477ea2fe679fb80
juuso-j <juuso.jokiniemi@turku.fi>
2023-12-05 09:23:20 +0200

    Disable deletion of event area statistics

--- parkings/admin.py
+++ parkings/admin.py
@@ -139,6 +139,9 @@ class EventAreaStatisticsAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
+    def has_delete_permission(self, request, obj=None):
+        return False
+
 
 @admin.register(ParkingCheck)
 class ParkingCheckAdmin(ReadOnlyAdmin, OSMGeoAdmin):
