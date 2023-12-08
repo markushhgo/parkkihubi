5fa8a036dbcb5915a822a356f61a48cd181bf887
juuso-j <juuso.jokiniemi@turku.fi>
2023-12-08 11:23:10 +0200

    Add tests

--- parkings/tests/api/operator/test_event_parking.py
+++ parkings/tests/api/operator/test_event_parking.py
@@ -31,6 +31,16 @@ def new_event_parking_data():
 
 
 @pytest.fixture
+def updated_event_parking_data():
+    return {
+        'registration_number': 'ABC-123',
+        'time_start': '2016-12-10T20:34:38Z',
+        'time_end': '2016-12-10T23:33:29Z',
+        'location': {'coordinates': [24.90, 60.16], 'type': 'Point'},
+    }
+
+
+@pytest.fixture
 def event_parking_data_outside_event_areas():
     return {
         'registration_number': 'VSM-162',
@@ -100,7 +110,7 @@ def test_event_parking_required_fields(operator_api_client, event_parking):
                           expected_required_fields, detail_endpoint=True)
 
 
-def test_post_event_parking_with_event_area_id(operator_api_client, new_event_parking_data, event_area):
+def test_post_event_parking_with_event_area_id_and_location(operator_api_client, new_event_parking_data, event_area):
     new_event_parking_data['event_area_id'] = str(event_area.id)
     response_data = post(operator_api_client, list_url, new_event_parking_data)
     check_response_data(new_event_parking_data, response_data)
@@ -109,9 +119,33 @@ def test_post_event_parking_with_event_area_id(operator_api_client, new_event_pa
     assert response_data['event_area_id'] == new_event_parking_data['event_area_id']
 
 
+def test_post_event_parking_only_with_event_area_id(operator_api_client, new_event_parking_data, event_area):
+    new_event_parking_data.pop('location')
+    new_event_parking_data.pop('time_end')
+    new_event_parking_data['event_area_id'] = str(event_area.id)
+    response_data = post(operator_api_client, list_url, new_event_parking_data)
+    check_response_data(new_event_parking_data, response_data)
+    new_event_parking = EventParking.objects.get(id=response_data['id'])
+    assert new_event_parking.event_area.id == event_area.id
+    assert response_data['event_area_id'] == new_event_parking_data['event_area_id']
+
+
 def test_post_event_parking_with_non_existing_event_area_id(operator_api_client, new_event_parking_data):
     new_event_parking_data['event_area_id'] = '5097a7b1-2bf1-4a69-be8a-669f300d993e'
     post(operator_api_client, list_url, new_event_parking_data, 400)
+
+
+def test_put_event_parking(operator_api_client, event_parking, updated_event_parking_data):
+    detail_url = get_detail_url(event_parking)
+    assert EventParking.objects.count() == 1
+    response_data = put(operator_api_client, detail_url, updated_event_parking_data)
+    assert EventParking.objects.count() == 1
+    # check data in the response
+    check_response_data(updated_event_parking_data, response_data)
+
+    # check the actual object
+    event_parking.refresh_from_db()
+    check_data_matches_object(updated_event_parking_data, event_parking)
 
 
 def test_patch_event_parking_event_area_id(operator_api_client, complete_event_parking, event_area_factory):
@@ -162,6 +196,12 @@ def test_post_event_parking_without_event_area(operator_api_client, event_parkin
     event_parking_data_outside_event_areas['event_area_id'] = event_area.id
     post(operator_api_client, list_url, event_parking_data_outside_event_areas, 201)
     assert EventParking.objects.count() == 1
+
+
+def test_post_event_parking_without_event_area_and_location(operator_api_client, new_event_parking_data):
+    new_event_parking_data.pop('location')
+    post(operator_api_client, list_url, new_event_parking_data, 400)
+    assert EventParking.objects.count() == 0
 
 
 def test_patch_event_parking(operator_api_client, event_parking):
