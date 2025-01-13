25195a8c2fb04d535ab5ef95d4b51d176de20ada
juuso-j <68938778+juuso-j@users.noreply.github.com>
2025-01-13 07:54:26 +0200

    Add parking_anonymized endpoint

---
+++ parkings/api/data/parking_anonymized.py
@@ -0,0 +1,35 @@
+
+import django_filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import serializers, viewsets
+
+from parkings.models import Parking
+from parkings.pagination import Pagination
+
+from .permissions import IsDataUser
+
+
+class ParkingAnonymizedFilterSet(django_filters.FilterSet):
+
+    class Meta:
+        model = Parking
+        fields = {
+            'time_start': ['lte', 'gte'],
+        }
+
+
+class ParkingAnonymizedSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Parking
+        exclude = ['registration_number', 'normalized_reg_num']
+
+
+class ParkingAnonymizedViewSet(viewsets.ReadOnlyModelViewSet):
+
+    queryset = Parking.objects.all()
+    serializer_class = ParkingAnonymizedSerializer
+    pagination_class = Pagination
+    permission_classes = [IsDataUser]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ParkingAnonymizedFilterSet
--- parkings/api/data/urls.py
+++ parkings/api/data/urls.py
@@ -2,10 +2,12 @@ from rest_framework.routers import DefaultRouter
 
 from ..url_utils import versioned_url
 from .event_parking_anonymized import EventParkingAnonymizedViewSet
+from .parking_anonymized import ParkingAnonymizedViewSet
 
 router = DefaultRouter()
 
 router.register('event_parking_anonymized', EventParkingAnonymizedViewSet, basename='event_parking_anonymized')
+router.register('parking_anonymized', ParkingAnonymizedViewSet, basename='parking_anonymized')
 
 
 app_name = 'data'
